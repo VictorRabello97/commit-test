d8a11833f8427bf84ebc818fe3bc3749939a1b06
Victor Rabello97 <victorrabello97@gmail.com>
2023-08-27 21:20:58 -0300

    Implementando cookies e trabalhando com middlawares na aplicação

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 import fastify from 'fastify'
-import { knex } from './database'
-import { randomUUID } from 'crypto'
+import cookie from "@fastify/cookie"
 import { transactionsRoutes } from './routes/transactions'
 
 const app = fastify()
+
+app.register(cookie)
 
 app.register(transactionsRoutes, {
   prefix: 'transactions'
